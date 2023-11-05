4a3e8fae21a102c74bb0734911caee597edaf766
Tyce Herrman <tyce.herrman@pm.me>
2023-11-04 20:44:47 -0400

    Add Crawler Application
    Crawl projects directory looking for .yamls and update dvc.yaml

---
+++ crawler/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System.IO;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+class Program {
+    static void Main(string[] args) {
+        string filePath = "../dvc.yaml";
+        
+        if (File.Exists(filePath)) {
+            File.Delete(filePath);
+        }
+
+        string projectPath = "../project";
+        string[] yamlFiles = Directory.GetFiles(projectPath, "*.yaml", SearchOption.AllDirectories);
+        
+        var graphSpec = new Dictionary<string, Dictionary<string, object>>();
+        graphSpec["stages"] = new Dictionary<string, object>();
+
+        foreach (string yamlFile in yamlFiles) {
+            string yamlFilePath = Path.GetDirectoryName(yamlFile).Replace("/", "-").Replace("..-project-", "");
+            var deserializer = new DeserializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
+            var yamlObject = deserializer.Deserialize<Dictionary<string, object>>(new StreamReader(yamlFile));
+            string rhinoFilePath = yamlFilePath.Replace(".gh.yaml", ".3dm");
+            yamlObject["cmd"] = "../RhinoRunner/bin/Debug/RhinoRunner.exe -File " + rhinoFilePath;
+            graphSpec["stages"][yamlFilePath] = yamlObject;
+        }
+
+        var serializer = new SerializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
+        var serializedYaml = serializer.Serialize(graphSpec);
+        Console.WriteLine(serializedYaml);
+        using (StreamWriter writer = new StreamWriter(filePath, true)) {
+            writer.WriteLine(serializedYaml);
+        }
+    }
+}
+
